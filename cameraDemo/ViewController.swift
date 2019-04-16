2f45f73daa455d3effe4037d4f58df851c606fb5
admin <mobileteam@syneoteksoft.com>
2019-04-16 11:11:19 +0530

    first commit

---
+++ cameraDemo/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  cameraDemo
+//
+//  Created by admin on 09/04/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    var imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //TODO: IMAGE BASE_64 CONVERSION
+    func convertImageToBase64(image: UIImage) -> String {
+        let imageData = image.pngData()!
+        return imageData.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
+    }
+    @IBAction func clickToPhoto(_ sender: UIButton) {
+        let alert:UIAlertController=UIAlertController(title: "Choose Image", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
+        let cameraAction = UIAlertAction(title: "Camera", style: UIAlertAction.Style.default) {
+            UIAlertAction in
+            self.openCamera(UIImagePickerController.SourceType.camera)
+        }
+        let gallaryAction = UIAlertAction(title: "Gallary", style: UIAlertAction.Style.default) {
+            UIAlertAction in
+            self.openCamera(UIImagePickerController.SourceType.photoLibrary)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) {
+            UIAlertAction in
+        }
+        
+        // Add the actions
+        imagePicker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        alert.addAction(cameraAction)
+        alert.addAction(gallaryAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera(_ sourceType: UIImagePickerController.SourceType) {
+        imagePicker.sourceType = sourceType
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK:UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imageView.image = img
+        print(img!)
+        imagePicker.dismiss(animated: true, completion: nil)
+        let myImage = img!.resizeWithWidth(width: 100)!
+        let newImg = convertImageToBase64(image: myImage)
+        print(newImg)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("imagePickerController cancel")
+    }
+}
+
+extension UIImage{
+    func resizeWithWidth(width: CGFloat) -> UIImage? {
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = self
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        guard let result = UIGraphicsGetImageFromCurrentImageContext() else { return nil }
+        UIGraphicsEndImageContext()
+        return result
+    }
+}
